a5d8ebf3a8596a6b9c8e6453f44ecf4a5bdceed3
rfrech <rfrechette@cnm.edu>
2020-10-07 09:25:23 -0700

    replaced thread w/ Asynctask.

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.animals.controller;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebResourceRequest;
@@ -18,10 +19,10 @@ import edu.cnm.deepdive.animals.model.ApiKey;
 import edu.cnm.deepdive.animals.service.AnimalService;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
 
 public class ImageFragment extends Fragment {
 
@@ -30,7 +31,6 @@ public class ImageFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-
     View root = inflater.inflate(R.layout.fragment_image, container, false);
     setupWebView(root);
     return root;
@@ -51,26 +51,28 @@ public class ImageFragment extends Fragment {
     settings.setDisplayZoomControls(false);
     settings.setUseWideViewPort(true);
     settings.setLoadWithOverviewMode(true);
-    new Retriever().start();
+    new RetrieverTask().execute();
   }
 
-  private class Retriever extends Thread {
+  private class RetrieverTask extends AsyncTask<Void, Void, List<Animal>> {
+
+    private AnimalService animalService;
 
     @Override
-    public void run() {
-
+    protected void onPreExecute() {
+      super.onPreExecute();
       Gson gson = new GsonBuilder()
           .create();
-
       Retrofit retrofit = new Retrofit.Builder()
           .baseUrl("https://us-central1-apis-4674e.cloudfunctions.net/")
           .addConverterFactory(GsonConverterFactory.create(gson))
           .build();
-
       AnimalService animalService = retrofit.create(AnimalService.class);
+    }
 
+    @Override
+    protected List<Animal> doInBackground(Void... voids) {
       try {
-
         Response<ApiKey> keyResponse = animalService.getApiKey().execute();
         ApiKey key = keyResponse.body();
         assert key != null;
@@ -79,20 +81,26 @@ public class ImageFragment extends Fragment {
         Response<List<Animal>> listResponse = animalService.getAnimals(clientKey).execute();
         List<Animal> animalList = listResponse.body();
         assert animalList != null;
-        final String imageUrl = animalList.get(0).getImageURL();
-
-        getActivity().runOnUiThread(new Runnable() {
-          @Override
-          public void run() {
-            contentView.loadUrl(imageUrl);
-          }
-        });
-
-      } catch (IOException e) {
+        return animalList;
+      } catch (
+          IOException e) {
         Log.e("AnimalService", e.getMessage(), e);
+        cancel(true);
       }
-
+      return null;
     }
 
+    @Override
+    protected void onPostExecute(List<Animal> animalList) {
+      final String imageUrl = animalList.get(22).getImageURL();
+
+      Objects.requireNonNull(getActivity()).runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          contentView.loadUrl(imageUrl);
+        }
+      });
+
+    }
   }
 }

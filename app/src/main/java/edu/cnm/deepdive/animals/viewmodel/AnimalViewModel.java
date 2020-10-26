839e39e22a869a8cd8f02c93dce4d4871c762d3b
rfrech <rfrechette@cnm.edu>
2020-10-26 10:16:11 -0600

    floobits..

--- app/src/main/java/edu/cnm/deepdive/animals/viewmodel/AnimalViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/animals/viewmodel/AnimalViewModel.java
@@ -2,31 +2,24 @@ package edu.cnm.deepdive.animals.viewmodel;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
-import android.os.AsyncTask;
-import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.Lifecycle.Event;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import edu.cnm.deepdive.animals.BuildConfig;
+import androidx.lifecycle.OnLifecycleEvent;
 import edu.cnm.deepdive.animals.model.Animal;
-import edu.cnm.deepdive.animals.model.ApiKey;
-import edu.cnm.deepdive.animals.service.AnimalService;
-import io.reactivex.schedulers.Schedulers;
-import java.io.IOException;
+import edu.cnm.deepdive.animals.service.AnimalsRepository;
+import io.reactivex.disposables.CompositeDisposable;
 import java.util.List;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AnimalViewModel extends AndroidViewModel {
 
   private final MutableLiveData<List<Animal>>animals;
   private final MutableLiveData<Integer> selectedItem;
   private final MutableLiveData<Throwable> throwable;
-  private final AnimalService animalService;
+  private final AnimalsRepository animalsRepository;
+  private  final CompositeDisposable pending;
 
   public AnimalViewModel(
       @NonNull Application application) {
@@ -34,7 +27,9 @@ public class AnimalViewModel extends AndroidViewModel {
     animals = new MutableLiveData<>();
     selectedItem = new MutableLiveData<>();
     throwable = new MutableLiveData<>();
-    animalService = AnimalService.getInstance();
+//    animalService = AnimalService.getInstance();
+    animalsRepository = new AnimalsRepository(application);
+    pending = new CompositeDisposable();
     loadAnimals();
   }
 
@@ -56,14 +51,17 @@ public class AnimalViewModel extends AndroidViewModel {
 
   @SuppressLint("CheckResult")
   private void loadAnimals() {
-
-    animalService.getApiKey()
-        .subscribeOn(Schedulers.io())
-        .flatMap((key) -> animalService.getAnimals(key.getKey()))
+    pending.add(
+      animalsRepository.loadAnimals()
         .subscribe(
             animals::postValue,
             throwable::postValue
-        );
+        )
+    );
 
+  }
+  @OnLifecycleEvent(Event.ON_STOP)
+  private void clearPending(){
+    pending.clear();
   }
 }
